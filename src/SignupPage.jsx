559cf386f537f4bc61fa660f1bef6954b0d5f678
Akhilsai <codeakhilsai@gmail.com>
2026-03-07 17:35:11 +0530

    gap removed

--- src/SignupPage.jsx
+++ src/SignupPage.jsx
@@ -31,7 +31,6 @@ const SignupPage = () => {
       );
       const user = userCredential.user;
 
-      // ✅ Send verification email
       await sendEmailVerification(user);
       setMessage(
         "Verification email sent! Please check your inbox and verify your email before continuing."
@@ -66,27 +65,19 @@ const SignupPage = () => {
   };
 
   useEffect(() => {
-    document.body.style.overflow = "hidden";
-    return () => {
-      document.body.style.overflow = "";
-    };
-  }, []);
-
-  useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, async (user) => {
       if (user) {
-        // 🔁 Keep checking every few seconds if the user verifies
         const interval = setInterval(async () => {
-          await user.reload(); // Refresh user data
+          await user.reload();
           if (user.emailVerified) {
-            clearInterval(interval); // stop checking
+            clearInterval(interval);
             await setDoc(doc(db, "Users", user.uid), {
               email: user.email,
               createdAt: new Date(),
             });
             navigate("/ThreatDetectionPage");
           }
-        }, 3000); // checks every 3 seconds
+        }, 3000);
 
         return () => clearInterval(interval);
       }
@@ -96,9 +87,9 @@ const SignupPage = () => {
   }, [navigate]);
 
   return (
-    <div className="min-h-screen w-screen bg-linear-to-br from-slate-900 via-purple-900 to-slate-900 flex items-center justify-center">
+    <div className="min-h-screen w-full bg-linear-to-br from-slate-900 via-purple-900 to-slate-900 flex items-center justify-center overflow-auto md:overflow-hidden">
+      {" "}
       <div className="absolute inset-0 bg-[url('data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNjAiIGhlaWdodD0iNjAiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGRlZnM+PHBhdHRlcm4gaWQ9ImdyaWQiIHdpZHRoPSI2MCIgaGVpZ2h0PSI2MCIgcGF0dGVyblVuaXRzPSJ1c2VyU3BhY2VPblVzZSI+PHBhdGggZD0iTSAxMCAwIEwgMCAwIDAgMTAiIGZpbGw9Im5vbmUiIHN0cm9rZT0icmdiYSgyNTUsMjU1LDI1NSwwLjAzKSIgc3Ryb2tlLXdpZHRoPSIxIi8+PC9wYXR0ZXJuPjwvZGVmcz48cmVjdCB3aWR0aD0iMTAwJSIgaGVpZ2h0PSIxMDAlIiBmaWxsPSJ1cmwoI2dyaWQpIi8+PC9zdmc+')] opacity-20"></div>
-
       <div className="relative w-full max-w-md">
         <div className="absolute -top-20 -left-20 w-40 h-40 bg-cyan-500 rounded-full mix-blend-multiply filter blur-xl opacity-20 animate-pulse"></div>
         <div
